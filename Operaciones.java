63fe740bcf89d7b7ae8e3362f2b4e99937a84ecf
Luis Daniel <70388347+danieldrako@users.noreply.github.com>
2023-08-24 12:34:53 -0600

    add 05Operaciones

--- Operaciones.java
+++ Operaciones.java
@@ -3,6 +3,7 @@ public class Operaciones {
         int numero_uno = 4;
         int numero_dos = 4;
         int resultado  = 0;
+        
         resultado = numero_uno + numero_dos / 2;
 
         System.out.println("El resultado es: " + resultado);
