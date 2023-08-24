ba64871abf4cf96648181c3658fcfb99b7cb4329
Luis Daniel <70388347+danieldrako@users.noreply.github.com>
2023-08-24 12:27:43 -0600

    add 04Suma

---
+++ Suma.java
@@ -0,0 +1,10 @@
+public class Suma {
+    public static void main(String args[]){
+        int numero_uno = 5;
+        int numero_dos = 3;
+        int resultado  = 0;
+
+        resultado = numero_uno + numero_dos;
+        System.out.println("E valor de tu suma es: " + resultado);
+    }
+}
