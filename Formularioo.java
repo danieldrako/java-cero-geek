e3918b79869b4a2b06bddf1999e9c977f364d7bf
Luis Daniel <70388347+danieldrako@users.noreply.github.com>
2023-09-06 09:03:52 -0600

    add 25 Formularioo user interface

---
+++ Formularioo.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Formularioo extends JFrame{
+	
+    public Formularioo(){
+        setLayout(null);
+        
+    }
+    public static void main(String args[]){
+        Formularioo formulario1 = new Formularioo();
+        formulario1.setBounds(0,0,400,550);
+        formulario1.setVisible(true);
+        formulario1.setLocationRelativeTo(null);
+        formulario1.setResizable(false);
+        
+    }  
+}
